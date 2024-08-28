19ad2b7742180f57fd7e52e2d030f70c84687734
Amir Ali <aali@techtiz.co>
2024-08-28 20:13:02 +0500

    Fixing this style issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './../module/lib/theme'; // Adjust the import path
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
   return (
