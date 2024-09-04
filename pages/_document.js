49bbf3b05c6017213cea13d4a6a243e1c8eaf349
Amir Ali <aali@techtiz.co>
2024-09-04 12:23:23 +0500

    fixing the  global style issue

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,102 @@
-import React from 'react';
+// pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheets } from '@material-ui/core/styles';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html>
         <Head>
-          {/* Import Google Fonts here */}
-          <link
-            href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
+          <style>
+            {`
+              html,
+              body {
+                padding: 0;
+                margin: 0;
+                font-family:
+                  Inter,
+                  -apple-system,
+                  BlinkMacSystemFont,
+                  Segoe UI,
+                  Roboto,
+                  Oxygen,
+                  Ubuntu,
+                  Cantarell,
+                  Fira Sans,
+                  Droid Sans,
+                  Helvetica Neue,
+                  Barlow-Medium,
+                  Quicksand-Bold,
+                  Barlow-Regular,
+                  sans-serif;
+              }
+
+              a {
+                color: inherit;
+                text-decoration: none;
+              }
+
+              * {
+                box-sizing: border-box;
+              }
+
+              img {
+                max-width: 100%;
+                height: auto;
+              }
+
+              :root {
+                --foreground-rgb: 0, 0, 0;
+                --background-start-rgb: 214, 219, 220;
+                --background-end-rgb: 255, 255, 255;
+              }
+
+              @media (prefers-color-scheme: dark) {
+                :root {
+                  --foreground-rgb: 255, 255, 255;
+                  --background-start-rgb: 0, 0, 0;
+                  --background-end-rgb: 0, 0, 0;
+                }
+              }
+
+              body {
+                color: rgb(var(--foreground-rgb));
+                background: linear-gradient(
+                    to bottom,
+                    transparent,
+                    rgb(var(--background-end-rgb))
+                  )
+                  rgb(var(--background-start-rgb));
+              }
+
+              body {
+                background-color: red;
+              }
+
+              @layer utilities {
+                .text-balance {
+                  text-wrap: balance;
+                }
+              }
+
+              @font-face {
+                font-family: "Barlow-Medium";
+                src: local("Barlow-Medium"),
+                     url("/Barlow-Medium.ttf") format("truetype");
+              }
+
+              @font-face {
+                font-family: "Barlow-Regular";
+                src: local("Barlow-Regular"),
+                     url("/Barlow-Regular.ttf") format("truetype");
+              }
+
+              @font-face {
+                font-family: "Quicksand-Bold";
+                src: local("Quicksand-Bold"),
+                     url("/Quicksand-Bold.ttf") format("truetype");
+              }
+            `}
+          </style>
         </Head>
         <body>
           <Main />
@@ -26,19 +107,4 @@ export default class MyDocument extends Document {
   }
 }
 
-MyDocument.getInitialProps = async (ctx) => {
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
-
-  ctx.renderPage = () => originalRenderPage({
-    enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-  });
-
-  const initialProps = await Document.getInitialProps(ctx);
-
-  return {
-    ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
-  };
-};
+export default MyDocument;
