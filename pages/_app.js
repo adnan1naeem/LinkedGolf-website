024cfad2e6f1e9199cb4ab2504735ed1939e344a
Amir Ali <aali@techtiz.co>
2024-09-04 12:02:53 +0500

    adding scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,20 @@
-import Head from "next/head";
-import "../styles/global.css";
-import React from "react";
-
+import React, { useEffect } from 'react';
+import Head from 'next/head';
+import '../styles/global.scss';
 
 export default function App({ Component, pageProps }) {
-  React.useEffect(() => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }, []);
-  
+
   return (
     <div>
       <Head>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
       <Component {...pageProps} />
       <style jsx global>{`
@@ -22,7 +22,6 @@ export default function App({ Component, pageProps }) {
         body {
           padding: 0;
           margin: 0;
-
           font-family: sans-serif;
         }
         * {
